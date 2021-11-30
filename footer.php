ffcfffb8e9008118fb825d17f155cbe30292b0cc
Yggdrasil16 <85646207+Yggdrasil16@users.noreply.github.com>
2021-11-30 18:35:51 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,43 @@
+<!--
+<footer class="footer">
+    <div id="we" class="footer-child"><h3>Qui sommes-nous ?</h3>
+    <p>Nous Sommes 2 pauvres collégiens obligés à faire un site web (à changer bien sûr)</p>
+    </div>
+    <div id="info" class="footer-child">
+        <h3>Informations</h3>
+        <p><a href="./index.php">Accueil</a><br/>
+        <a href="./autheurs.php">Les Autheurs</a><br/>
+        <a href="sitemap.xml">Plan du site</a><br/>
+        <a href="./other/">Des mêmes autheurs</a><br/>
+        Ce site est sous la license <a href="./LICENSE.md">GPL</a>
+    </div>
+    <div id="contact" class="footer-child">
+        <h3>Nous Contacter</h3>
+        <p>
+            Par Mail: <a href="mailto:mail@example.com">Pierre-Yves</a>, <a href="mailto:mail@example.com">Joachim</a><br/>
+            Par discord: <a href="https://discord.gg/FH5jSTwt9Q"><img src="./discord-logo.jpg" id="discord-logo" alt="logo de discord"/></a>
+        </p>
+    </div>
+</footer>
+-->
+<footer class="footer">
+    <div id="we" class="footer-child"><h3>Qui sommes-nous ?</h3>
+    <p>Nous Sommes 2 collégiens en classe de 3e ayant pour projet de faire un site pour sensibilisé sur la question de l'écologie, en effet, c'est un problème croissant, nous avons du mal à nous en rendrde compte mais, nous sommes en train de détruire notre lieu de vie</p>
+    </div>
+    <div id="info" class="footer-child">
+        <h3>Informations</h3>
+        <p><a href="<?php echo $dir.'index.php' ?>">Accueil</a><br/>
+
+        <a href="<?php echo $dir ?>sitemap.xml">Plan du site</a><br/>
+        <a href="<?php echo $dir ?>other/">Des mêmes auteurs</a><br/>
+        Ce site est sous la license <a href="<?php echo $dir ?>LICENSE.md">GPL</a><br/>
+	<a href="#">Code Source</a>
+    </div>
+    <div id="contact" class="footer-child">
+        <h3>Nous Contacter</h3>
+        <p>
+            Par Mail: <a href="mailto:mail@example.com">Pierre-Yves</a>, <a href="mailto:mail@example.com">Joachim</a><br/>
+            Par discord: <a href="https://discord.gg/FH5jSTwt9Q" id="link-discord" title="Voir le serveur être écolo ou ne pas être écolo ?"><img src="./discord-logo.jpg" id="discord-logo" alt="logo de discord"/></a>
+        </p>
+    </div>
+</footer>   
---
+++ header.php
@@ -0,0 +1,3 @@
+<meta charset="utf-8"/>
+<link rel="stylesheet" type="text/css" href="<?php echo $dir ?>style.css"/>
+<link rel="icon" type="image/png" href="<?php echo $dir ?>favicon.png"/>
