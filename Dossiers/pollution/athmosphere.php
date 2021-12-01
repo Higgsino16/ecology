1a54be426131e1eab5823b82cf0f3d336ccb8273
Higgsino16 <85646207+Higgsino16@users.noreply.github.com>
2021-12-01 21:20:05 +0100

    Add files via upload

---
+++ Dossiers/pollution/athmosphere.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <title>Pollution athmosphérique</title>
+    <link rel='stylesheet' type='text/css' href='../../style.css'/>
+    <link rel="icon" type="image/png" href="../../favicon.png"/>
+</head>
+<body>
+<nav id="wrap" class="nav">
+        <ul class="navbar">
+          <li>
+            <a href="../index.html" id="link-home">Nous rêvons d'un monde plus vert</a>
+          </li>
+          <li>
+            <a href="./Dossiers/pollution">Pollution</a>
+            <ul>
+              <li><a href="./Dossiers/pollution/plastique.php">Plastique</a></li>
+              <li><a href="./Dossiers/pollution/athmosphere.php">Pollution athmosphérique</a></li>
+              <li><a href="./Dossiers/pollution/sols.php">Pollution des sols</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="rechauffement/">Réchauffement climatique</a>
+            <ul>
+              <li><a href="./Dossiers/rechauffement/glaces.php" >Fonte des Glaces</a></li>
+              <li><a href="./Dossiers/rechauffement/inondations.php">Inondation</a></li>
+              <li style="display: none;"><a href="#">#</a></li>
+            </ul>
+          </li>
+        </ul>
+    
+    </nav>
+
+    <section class="section">
+      <p>du text</p>
+    </section>
+    <footer class="footer">
+    <div id="we" class="footer-child"><h3>Qui sommes-nous ?</h3>
+    <p>Nous Sommes 2 collégiens en classe de 3e ayant pour projet de faire un site pour sensibilisé sur la question de l'écologie, en effet, c'est un problème croissant, nous avons du mal à nous en rendrde compte mais, nous sommes en train de détruire notre lieu de vie</p>
+    </div>
+    <div id="info" class="footer-child">
+        <h3>Informations</h3>
+        <p><a href="<?php echo $dir.'index.php' ?>">Accueil</a><br/>
+
+        <a href="sitemap.xml">Plan du site</a><br/>
+        <a href="./other/">Des mêmes auteurs</a><br/>
+        Ce site est sous la license <a href="./LICENSE.md">GPL</a><br/>
+	<a href="#">Code Source</a>
+    </div>
+    <div id="contact" class="footer-child">
+        <h3>Nous Contacter</h3>
+        <p>
+            Par Mail: <a href="mailto:mail@example.com">Pierre-Yves</a>, <a href="mailto:mail@example.com">Joachim</a><br/>
+            Par discord: <a href="https://discord.gg/FH5jSTwt9Q" id="link-discord" title="Voir le serveur être écolo ou ne pas être écolo ?"><img src="./discord-logo.jpg" id="discord-logo" alt="logo de discord"/></a>
+        </p>
+    </div>
+</footer>  
+</body>
+</html>
---
+++ Dossiers/pollution/plastique.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <title>Pollution plastique</title>
+    <link rel='stylesheet' type='text/css' href='../style.css'>
+    </head>
+<body>
+    <nav id="wrap" class="nav">
+        <ul class="navbar">
+          <li>
+            <a href="../index.html" id="link-home">Nous rêvons d'un monde plus vert</a>
+          </li>
+          <li>
+            <a href="./">Pollution</a>
+            <ul>
+              <li><a href="plastique.html">Plastique</a></li>
+              <li><a href="athmosphere.html">Pollution athmosphérique</a></li>
+              <li><a href="sols.html">Pollution des sols</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">Réchauffement climatique</a>
+            <ul>
+              <li><a href="#" >Fonte des Glaces</a></li>
+              <li><a href="#">Inondation</a></li>
+              <li><a href="#">#</a></li>
+            </ul>
+          </li>
+        </ul>
+    
+    </nav>
+</body>
+</html>
